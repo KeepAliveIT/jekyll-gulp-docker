b18f0e2ff3d2c1a2c436cee2b454219e6d8f2724
Keep-Alive I.T <joel@keepaliveit.co.uk>
2020-10-01 14:26:42 +0100

    Permissions driving me crazzzzyyy

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 # Run on container launch to ensure we have a clean state of jekyll and node libraries.
 chown jekyll:jekyll -R /srv/jekyll
+chmod -R 777 /srv/jekyll
 bundle install --quiet
 npm install --save-dev --quiet
 
